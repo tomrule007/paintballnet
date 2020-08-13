44203d36b3cac05c5d9a39d857cf4628f53ce778
Thomas Herzog <Tomrule007@gmail.com>
2020-08-13 14:53:28 -0700

    Add game ready scripts

---
+++ gameReadyStandby.py
@@ -0,0 +1,20 @@
+def send_single_char(string):
+    for c in string:
+        keyboard.send_keys(c)
+        
+def send_command(commands):
+    keyboard.send_keys(" ") #open chat/command prompt
+    for cmd in commands:
+        send_single_char(cmd) # send command one character at a time.
+        keyboard.send_keys("<enter>") # submit command   
+    keyboard.send_keys("<escape>") #close chat/command prompt
+
+try:
+    enabled = store.get_global_value("enabled")
+except TypeError:
+    enabled = True #default setting
+
+if enabled: 
+    send_command(["/game ready standby"])
+else:
+    keyboard.send_keys('<shift>+q')
